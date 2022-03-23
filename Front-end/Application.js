65d1b394baa7ce7e74c88a5d62fdae76e43bface
GamGyu <barry0105@Naver.com>
2022-03-23 19:47:14 +0900

    hi?

--- Front-end/Application.js
+++ Front-end/Application.js
@@ -1,1 +1,25 @@
-unKnown
+bt_to_board = document.querySelector("#to_board")
+bt_to_question = document.querySelector("#to_question");
+bt_to_main = document.querySelector("#backtomain");
+
+question_display = document.getElementById("question");
+main_display = document.getElementById("main");
+board_display = document.getElementById("board");
+
+bt_to_question.addEventListener("click",function(){
+main_display.style.display = "none";
+question_display.style.display = "block";
+bt_to_main.style.display = "block";
+});
+
+bt_to_board.addEventListener("click",function(){
+    main_display.style.display = "none";
+    board_display.style.display = "block";
+    bt_to_main.style.display = "block";
+})
+
+bt_to_main.addEventListener("click", function(){
+    main_display.style.display = "block";
+    board_display.style.display = "none";
+    question_display.style.display = "none";
+})
