cab838a4aa0480927a4957162cbc4b7a005659ba
GamGyu <barry0105@Naver.com>
2022-09-02 14:40:15 +0900

    0902

--- Front-end/Main_Site/src/component/Body/info.js
+++ Front-end/Main_Site/src/component/Body/info.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function info(props) {
     return (
-        <div className='info'>
+        <InfoUs>
             <ul>
                 <li>프로젝트명 : INBTI</li>
                 <li>설명 : INBTI는 학생들의 진로 고민을 해결해주기 위해 고안한 진로 도우미 입니다.</li>
                 <li>사용 기술 : REACT, JAVASCRIPT, SPRING, FIREBASE, MARIADB 등</li>
                 <li>팀원 : 김규진, 김채은, 방주현, 전해린</li>
                 <li>
-                    <table border={1} width={500} >
+                    <table>
                         <tbody>
                         <tr>
                             <td>학번</td>
@@ -38,12 +39,46 @@ function info(props) {
                         </tr>
                         </tbody>
                         
-                    </table>
+                    </table>    
                 </li>
             </ul>
             
-        </div>
+        </InfoUs>
     );
 }
-
+const InfoUs = styled.div`
+    display: flex;
+    flex: 1;
+    background-color:rgb(187,223,219);
+    justify-content: center;
+    align-items: flex-start;
+    height: 80vh;
+    max-height: 70vh;
+    overflow-y: auto;
+    ul{
+        padding: 0;
+        display:flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    li{
+        width: fit-content;
+        list-style: none;
+        line-height: 300%;
+        table{
+            width: 50vw;
+            border: 1px solid black;
+            line-height: 200%;
+            border-collapse: collapse;
+            text-align: center;
+            th, td {
+                border: 1px solid black;
+              }
+        }
+    }
+    @media all and (max-width: 768px){
+        height: 100vh;
+        max-height: 100vh;
+    }
+`;
 export default info;
